664f5d67a4e19bc8300dfda4628ba7029c5d3989
Olegik <olegelend@yandex.ru>
2021-10-05 22:09:35 +0300

    commit_2

--- src/main/java/com/pages/BasePage.java
+++ src/main/java/com/pages/BasePage.java
@@ -1,42 +1,37 @@
 package com.pages;
 
+
 import com.codeborne.selenide.Condition;
-import io.qameta.allure.Step;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.$;
-import static org.testng.Assert.assertTrue;
 
 public abstract class BasePage {
 
-    private static final String MAIN_TILE = ".//div//*[@class='box mainhdr']";
-    private static final String BANK_TITLE = ".//div//*[@class='mainHeading']";
-    private static final String GO_HOME_BUTTON = "/html/body/div/div/div[1]/button[1]";
-    private static final String LOGOUT = "/html/body/div/div/div[1]/button[2]";
+    private final SelenideElement homeBtn = $(By.xpath("/html/body/div[1]/div/div[2]/ul[1]/li[1]/a"));
+    private final SelenideElement employeeListBtn = $(By.xpath("/html/body/div[1]/div/div[2]/ul[1]/li[3]/a"));
+    private final SelenideElement aboutBtn = $(By.xpath("/html/body/div[1]/div/div[2]/ul[1]/li[2]/a"));
 
-
-    @Step
-    public void checkMainTile() {
-        $(By.xpath(MAIN_TILE)).shouldBe(Condition.visible);
-        assertTrue($(By.xpath(BANK_TITLE)).getText().contains("XYZ Bank"), "Title isn't correct");
+    public BasePage goHome() {
+        homeBtn.click();
+        return this;
     }
 
-    @Step
-    public void goHome() {
-        $(By.xpath(GO_HOME_BUTTON)).click();
+    public BasePage goAbout() {
+        aboutBtn.click();
+        return this;
     }
 
-    @Step
-    public void logout() {
-        $(By.xpath(LOGOUT)).click();
+    public BasePage goEmployeeList() {
+        employeeListBtn.click();
+        return this;
     }
 
-    public boolean isElementDisplayed(By findBy) {
-        return $(findBy).isDisplayed();
+    public BasePage checkVisibility(SelenideElement el) {
+        el.shouldBe(Condition.visible);
+        return this;
     }
 
-    public void openDropDownAndChoose(By dropdown, String exactOption) {
-        $(dropdown).click();
-        $(By.xpath(exactOption)).click();
-    }
 
 }
